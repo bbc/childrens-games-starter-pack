df7a07ac1d541caea4a83ae80c3ac94352e96b4c
Binh Bui <binhbui7@gmail.com>
2015-06-03 18:05:09 +0100

    Added getting started text

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,4 +1,4 @@
 # You should place all your build steps in this script which will be run by our automated build pipeline
 # When you commit you can check the pipeline is working by checking the file below
 
-echo "Hello World!" >> gettingstarted.txt
+echo "Hello World! Edit me and commit to Git to see the automated build pipeline in action!" >> gettingstarted.txt
