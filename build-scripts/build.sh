e780b822975f48cb9d64244154b510bad3f74e56
Binh Bui <binhbui7@gmail.com>
2015-06-03 18:20:54 +0100

    Updated build script comments

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,4 +1,6 @@
 # You should place all your build steps in this script which will be run by our automated build pipeline
+# If you require any specific software to be installed please consult your BBC Technical Contact/Project Manager
+# If the build fails you should receive an email with failure information
 # When you commit you can check the pipeline is working by checking the file below
 
 echo "Hello World! Edit me and commit to Git to see the automated build pipeline in action!" >> gettingstarted.txt
