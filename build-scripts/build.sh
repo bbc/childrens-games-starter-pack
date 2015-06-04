3f8494b0812c02b0107f7d7a5689b6ae8a409fe9
Binh Bui <binhbui7@gmail.com>
2015-06-04 14:49:57 +0100

    Updates to gitignore and build intro

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,6 +1,6 @@
 # You should place all your build steps in this script which will be run by our automated build pipeline
 # If you require any specific software to be installed please consult your BBC Technical Contact/Project Manager
 # If the build fails you should receive an email with failure information
-# When you commit you can check the pipeline is working by checking the file below
+# When you commit you can check the pipeline is working by checking the gettingstarted.txt file on the Test environment (get the URL from TPM)
 
 echo "Hello World! Edit me and commit to Git to see the automated build pipeline in action!" >> gettingstarted.txt
