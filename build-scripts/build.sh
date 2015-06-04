efdd508a8bec9cc2d8420243b734b7e249321d59
Binh Bui <binhbui7@gmail.com>
2015-06-04 17:08:54 +0100

    Make output dir

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -3,4 +3,5 @@
 # If the build fails you should receive an email with failure information
 # When you commit the following line, you can check the pipeline is working by checking the gettingstarted.txt file on the Test environment (get the URL from TPM)
 
+mkdir output
 echo "Hello World! Edit me and commit to Git to see the automated build pipeline in action!" >> /output/gettingstarted.txt
