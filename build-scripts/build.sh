666a0f200b6a321ae363d595f0ff3d5607d2cf46
Binh Bui <binhbui7@gmail.com>
2015-07-27 13:44:19 +0100

    Updated info on build.sh

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,7 +1,11 @@
 # You should place all your build steps in this script which will be run by our automated build pipeline
-# Your build steps should build your game to an output folder as demonstrated below, this folder is then uploaded to our servers
+# This script is run from the project root - you can cd around as you need to
+# Your build steps should build your game to a root-level output folder as demonstrated below, this folder is then uploaded to our servers
 # If you require any specific software to be installed please consult your BBC Technical Lead/TPM
 # If the build fails you should receive an email with failure information
 # When you commit the following line, you can check the pipeline is working by checking the gettingstarted.txt file on the Test environment (get the URL from TPM)
 
 echo "Hello World! Edit me and commit to Git to see the automated build pipeline in action!" >> output/gettingstarted.txt
+
+
+
