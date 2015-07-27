06ec00610953f7934d0bd5dcbdd3cb4894d255f8
Binh Bui <binhbui7@gmail.com>
2015-07-27 11:19:16 +0100

    Removed output folder from git

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,4 +1,5 @@
 # You should place all your build steps in this script which will be run by our automated build pipeline
+# Your build steps should build your game to an output folder as demonstrated below, this folder is then uploaded to our servers
 # If you require any specific software to be installed please consult your BBC Technical Lead/TPM
 # If the build fails you should receive an email with failure information
 # When you commit the following line, you can check the pipeline is working by checking the gettingstarted.txt file on the Test environment (get the URL from TPM)
