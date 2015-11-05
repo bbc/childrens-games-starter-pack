1956c09f3a9df7665cf5b18116648e8836fe99e9
Kevin Mitchell <kevin.mitchell@bbc.co.uk>
2015-11-05 15:33:18 +0000

    Changed gettingstarted.txt for main.js

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -5,7 +5,7 @@
 # If the build fails you should receive an email with failure information
 # When you commit the following line, you can check the pipeline is working by checking the gettingstarted.txt file on the Test environment (get the URL from TPM)
 
-echo "Hello World! Edit me and commit to Git to see the automated build pipeline in action!" >> output/gettingstarted.txt
+echo "Hello World! Edit me and commit to Git to see the automated build pipeline in action!" >> output/main.js
 
 
 
