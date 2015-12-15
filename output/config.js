ccc317559454e122b2420355fd6e3c28c8aab512
Binh Bui <binhbui7@gmail.com>
2015-12-15 12:09:17 +0000

    Removed output folder from commit

--- output/config.js
+++
@@ -1,8 +0,0 @@
-define(function(require) {
-    "use strict";
-
-    return {
-        statsAppName: "cbbcOrCbeebies",
-        statsCounterName: "cbbcOrCbeebies.games.starter_pack_example.page"
-    };
-});
