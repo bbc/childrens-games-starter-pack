1c38e8893a7821b3ce231a8a1c052a19da31ca45
Binh Bui <binhbui7@gmail.com>
2015-12-15 12:14:32 +0000

    Fixed build script again

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -12,8 +12,8 @@
 # file (from src/*).
 
 # 'build' the src files to output directory
-mkdir ../output
-cp ../src/* ../output
+mkdir output
+cp src/* output
 
 # edit the string below and commit to see your changes reflected
 helloWorldFunc="function appendHelloWorld() { \
@@ -23,4 +23,4 @@ helloWorldFunc="function appendHelloWorld() { \
 }"
 
 # replaces the appendHelloWorld function in main.js with the one above
-sed "s/function appendHelloWorld() {}/$helloWorldFunc/g" ../output/main.js > ../output/main.tmp && mv ../output/main.tmp ../output/main.js
+sed "s/function appendHelloWorld() {}/$helloWorldFunc/g" output/main.js > output/main.tmp && mv output/main.tmp output/main.js
